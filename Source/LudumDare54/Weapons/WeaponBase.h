fe2b68afb94659b787b4810024c0c4f4410c35a7
Artyom Volkov <tricky.fat.cat@gmail.com>
2023-09-30 23:22:06 +0300

    Create base weapon actor

---
+++ Source/LudumDare54/Weapons/WeaponBase.h
@@ -0,0 +1,59 @@
+﻿// Made by Titled Goose Team during Ludum Dare 54
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "WeaponBase.generated.h"
+
+class AProjectileBase;
+
+UCLASS()
+class LUDUMDARE54_API AWeaponBase : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AWeaponBase();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	TSubclassOf<AProjectileBase> ProjectileClass = nullptr;
+
+	UPROPERTY(BlueprintReadWrite)
+	FVector TargetLocation = FVector::ZeroVector;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta=(ClampMin="0.25", ClampMax="15"))
+	float RateOfFire = 10.f;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta=(ClampMin="1", ClampMax="30"))
+	int32 ProjectilesPerShot = 1;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	int32 DamagePerProjectile = 5;
+
+	UFUNCTION(BlueprintCallable)
+	bool MakeShot();
+
+	UFUNCTION(BlueprintCallable)
+	bool StartShooting();
+
+	UFUNCTION(BlueprintCallable)
+	bool StopShooting();
+
+protected:
+	UPROPERTY(BlueprintReadOnly)
+	bool bIsShooting = false;
+
+	bool bCanShoot = true;
+
+	FTimerHandle ShotDelayTimer;
+
+	float ShotDelay = 1.0f;
+
+	void HandleShotDelayTimer();
+};
