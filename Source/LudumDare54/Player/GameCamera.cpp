cf0a111521734c8e28445721be5dc63e803a6c87
Artyom Volkov <tricky.fat.cat@gmail.com>
2023-09-30 18:15:35 +0300

    Create game camera

---
+++ Source/LudumDare54/Player/GameCamera.cpp
@@ -0,0 +1,20 @@
+﻿// Made by Titled Goose Team during Ludum Dare 54
+
+
+#include "GameCamera.h"
+
+#include "Camera/CameraComponent.h"
+
+
+AGameCamera::AGameCamera()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	UCameraComponent* CameraComp = GetCameraComponent();
+	CameraComp->SetProjectionMode(ECameraProjectionMode::Orthographic);
+	CameraComp->SetOrthoWidth(5250);
+	CameraComp->SetAspectRatio(1.777778);
+	CameraComp->SetRelativeRotation(FRotator(-45.0, -45.0, 0.0));
+	CameraComp->SetRelativeLocation(FVector(-2380, 2380, 3250));	
+}
+
