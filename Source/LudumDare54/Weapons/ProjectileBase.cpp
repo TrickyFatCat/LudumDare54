ec6fb29787f59f1974939a0015371ddcdc49ae38
Artyom Volkov <tricky.fat.cat@gmail.com>
2023-09-30 19:53:39 +0300

    Create base projectile class

---
+++ Source/LudumDare54/Weapons/ProjectileBase.cpp
@@ -0,0 +1,26 @@
+﻿// Made by Titled Goose Team during Ludum Dare 54
+
+
+#include "ProjectileBase.h"
+
+#include "GameFramework/ProjectileMovementComponent.h"
+
+
+AProjectileBase::AProjectileBase()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	ProjectileMovementComponent = CreateDefaultSubobject<UProjectileMovementComponent>("ProjectileMovement");
+}
+
+void AProjectileBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AProjectileBase::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
---
+++ Source/LudumDare54/Weapons/ProjectileBase.h
@@ -0,0 +1,28 @@
+﻿// Made by Titled Goose Team during Ludum Dare 54
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "ProjectileBase.generated.h"
+
+class UProjectileMovementComponent;
+
+UCLASS()
+class LUDUMDARE54_API AProjectileBase : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AProjectileBase();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+
+protected:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	TObjectPtr<UProjectileMovementComponent> ProjectileMovementComponent;
+};
