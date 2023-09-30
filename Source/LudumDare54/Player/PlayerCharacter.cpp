0ca21dff85ae120e7e5547a1a341460ec95424d0
Artyom Volkov <tricky.fat.cat@gmail.com>
2023-09-30 11:45:42 +0300

    Create player character class

---
+++ Source/LudumDare54/Player/PlayerCharacter.cpp
@@ -0,0 +1,31 @@
+﻿// Made by Titled Goose Team during Ludum Dare 54
+
+
+#include "PlayerCharacter.h"
+
+#include "LudumDare54/Components/HitPointsComponent.h"
+
+
+APlayerCharacter::APlayerCharacter()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	HitPointsComponent = CreateDefaultSubobject<UHitPointsComponent>("HitPoints");
+}
+
+void APlayerCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void APlayerCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void APlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
---
+++ Source/LudumDare54/Player/PlayerCharacter.h
@@ -0,0 +1,30 @@
+﻿// Made by Titled Goose Team during Ludum Dare 54
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "PlayerCharacter.generated.h"
+
+class UHitPointsComponent;
+
+UCLASS()
+class LUDUMDARE54_API APlayerCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	APlayerCharacter();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+protected:
+	UPROPERTY(BlueprintReadOnly, Category="Components")
+	TObjectPtr<UHitPointsComponent> HitPointsComponent = nullptr;
+};
